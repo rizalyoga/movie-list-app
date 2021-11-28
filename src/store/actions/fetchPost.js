755fdc27304e05e63b5dac3e27fb82a886e11970
rizalyoga <ach.rizal.yogaswara@gmail.com>
2021-11-28 23:44:57 +0700

    Scrolling not fixed

--- src/store/actions/fetchPost.js
+++ src/store/actions/fetchPost.js
@@ -1,16 +1,19 @@
 import axios from "axios";
+import allStore from "./index.js";
 
-export const fetchPost = () => {
+export const fetchPost = (page) => {
   return (dispacth) => {
+    dispacth(allStore.setLoading(true));
     axios
-      .get("https://api.themoviedb.org/3/movie/now_playing?api_key=519d809c83b6f9bbf372a2ed5633376d&language=en-US&page=1")
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=519d809c83b6f9bbf372a2ed5633376d&language=en-US&page=${page}`)
       .then(({ data }) => {
         // console.log(data);
         dispacth(setPost(data.results));
       })
       .catch((err) => {
         console.log(err);
-      });
+      })
+      .finally((_) => dispacth(allStore.setLoading(false)));
   };
 };
 
--- src/views/Componenst/cards.jsx
+++ src/views/Componenst/cards.jsx
@@ -12,7 +12,6 @@ const Cards = (props) => {
     <div className="bg-gray-200 ">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         {/* <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 text-center pb-6">LIST MOVIES</h2> */}
-
         <div className=" card mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8  ">
           {props.movies.map((movie, index) => (
             <div key={index} className="group relative cards ">
